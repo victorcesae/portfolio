77d9eacadf9cd1f02b11c4480a68d225bd4b2c38
victorcesae <victor.evangelista2010@gmail.com>
2023-03-12 04:18:16 -0300

    initial

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1600,
+    chunkSizeWarningLimit: 4000,
   },
+  optimizeDeps: {
+    include: ["react", "react-dom"],
+  },
+  base: "/portfolio/",
 });
